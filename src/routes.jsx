fa77b16771cfd852d6f2366bd1aadff43245a60e
WParedes2007 <wparedes-2020379@kinal.edu.gt>
2025-04-30 17:52:36 -0600

    Register Terminado

---
+++ src/routes.jsx
@@ -0,0 +1,9 @@
+import { Auth } from "./pages/auth";
+import { Navigate } from "react-router-dom";
+
+const routes = [
+    {path: '/auth', element: <Auth/>},
+    { path: "/", element: <Navigate to="/auth" /> },
+]
+
+export default routes
