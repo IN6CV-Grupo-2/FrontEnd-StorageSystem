a443d2097f3f92176960396b047670024e8bfed7
Jefferson P. <jportillo-2019644@kinal.edu.gt>
2025-05-03 15:33:56 -0600

    Actualizaciones en el diseno, falta mejorar y pulir

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from '../src/components/navbars/navbar.jsx'
-import Sidebar from '../src/components/dashboard/sidebar.jsx'
+import Navbar from "../src/components/navbars/navbar.jsx";
+import Sidebar from "../src/components/dashboard/sidebar.jsx";
 import AppRoutes from "./routes";
+import Footer from "../src/components/footer/footer.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="layout">
+      <div className="layout d-flex flex-column">
         <Navbar />
-        <div className="content">
-          <Sidebar />
+        <Sidebar />
+        <div className="content p-3 flex-grow-1">
           <AppRoutes />
         </div>
+        <Footer />
       </div>
     </BrowserRouter>
   );
--- src/components/navbars/navbar.jsx
+++ src/components/navbars/navbar.jsx
@@ -3,9 +3,28 @@ import icon from "../../assets/img/icon.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <img className="nav-icon" src={icon} alt="icon" />
-      <h1>Storage</h1>
+    <nav className="navbar navbar-dark bg-primary p-3">
+      <div className="d-flex align-items-center">
+        <button
+          className="btn btn-outline-light me-4"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#sidebarOffcanvas"
+          aria-controls="sidebarOffcanvas"
+        >
+          ☰
+        </button>
+
+        <div className="d-flex align-items-center ms-auto">
+          <h1 className="text-white fs-7 mb-0 ms-1">Storage</h1>
+          {/* <img
+            className="nav-icon ms-2"
+            src={icon}
+            alt="icon"
+            style={{ width: "50px", height: "50px" }}
+          /> */}
+        </div>
+      </div>
     </nav>
   );
 };
